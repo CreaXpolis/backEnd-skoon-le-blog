66e53d0d1fdafac7b566c574d3d7c4460131ff89
CreaXpolis <creawesomeness.lab@gmail.com>
2021-09-08 21:45:52 +0200

    totales

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '79480f661418b499d76163a325dcee1e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dfcd796f-310a-4dcc-99d8-8dd6b693445c'
+};
